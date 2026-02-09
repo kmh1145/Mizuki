504b8fa334fac84a774ba9fac0d697da79de5794
kmh1145 <zhurui20082019@163.com>
2026-02-10 00:28:56 +0800

    feat: 更新站点配置，修改站点URL和导航栏链接

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "昆明湖的博客",
 	subtitle: "Ciallo～(∠・ω< )⌒☆",
-	siteURL: "https://blog.nnyunidc.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.91vip.ink/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-20", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
@@ -263,22 +263,22 @@ export const navBarConfig: NavBarConfig = {
 			icon: "material-symbols:link",
 			children: [
 				{
-					name: "GitHub",
-					url: "https://github.com/matsuzaka-yuki/Mizuki",
+					name: "宁宁云",
+					url: "https://nnyunidc.com/",
 					external: true,
-					icon: "fa7-brands:github",
+					icon: "fa7-solid:server",
 				},
 				{
 					name: "Bilibili",
-					url: "https://space.bilibili.com/701864046",
+					url: "https://space.bilibili.com/471716423",
 					external: true,
 					icon: "fa7-brands:bilibili",
 				},
 				{
-					name: "Gitee",
-					url: "https://gitee.com/matsuzakayuki/Mizuki",
+					name: "统计",
+					url: "https://cloud.umami.is/share/HTVZr5HOQk5Fpe0O",
 					external: true,
-					icon: "mdi:git",
+					icon: "fa7-solid:chart-line",
 				},
 			],
 		},
